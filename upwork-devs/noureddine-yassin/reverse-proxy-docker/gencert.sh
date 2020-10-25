01fa6302778173be0e4a2a8e0c04ed93ea36c5ca
NourEddineX <noureddinex@protonmail.com>
2020-10-25 17:09:07 +0200

    combine server.* into full.pem

--- upwork-devs/noureddine-yassin/reverse-proxy-docker/gencert.sh
+++ upwork-devs/noureddine-yassin/reverse-proxy-docker/gencert.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 openssl genrsa -out server.key 2048
 echo -e '\n\n\n\n\n' | openssl req -new -out - -key server.key -config openssl.cnf 2> /dev/null | openssl x509 -req -days 3650 -in - -signkey server.key -out server.crt -extensions v3_req -extfile openssl.cnf 2> /dev/null
+cat server.key server.crt > full.pem
