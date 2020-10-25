e29fc2029dc4adc5ed10b9e7995d1971e93ff031
NourEddineX <noureddinex@protonmail.com>
2020-10-25 18:33:50 +0200

    OpenSSL edits

--- upwork-devs/noureddine-yassin/reverse-proxy-docker/gencert.sh
+++ upwork-devs/noureddine-yassin/reverse-proxy-docker/gencert.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-openssl genrsa -out server.key 2048
-echo -e '\n\n\n\n\n' | openssl req -new -out - -key server.key -config openssl.cnf 2> /dev/null | openssl x509 -req -days 3650 -in - -signkey server.key -out server.crt -extensions v3_req -extfile openssl.cnf 2> /dev/null
-echo -e '\n\n\n\n\n' |echo -e '\n\n\n\n\n' | openssl req -x509 -new -nodes -key server.key -sha256 -days 3650 -out server.ca -config openssl.cnf 
+echo -e '\n\n\n\n\n' | openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout server.key -out server.crt -config openssl.cnf
 cat server.key server.crt > full.pem
